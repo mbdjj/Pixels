71d742320255abfe11f3f82cceef2535ff88f4ef
Marcin Bartmiński <mbdjj@o2.pl>
2024-04-17 08:24:10 +0200

    Add drawing with drag gesture

--- Pixels/ContentView.swift
+++ Pixels/ContentView.swift
@@ -17,14 +17,28 @@ struct ContentView: View {
                 HStack(spacing: 0) {
                     ForEach(0 ..< 8, id: \.self) { index in
                         pixels[row][index]
-                            .onTapGesture {
-                                pixels[row][index] = .red
-                            }
                     }
                 }
             }
         }
         .aspectRatio(1, contentMode: .fit)
+        .overlay {
+            GeometryReader { proxy in
+                Rectangle().fill(.clear).contentShape(Rectangle())
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { value in
+                                let location = value.location
+                                let x = floor(location.x / proxy.size.width * 8)
+                                let y = floor(location.y / proxy.size.height * 8)
+                                
+                                if x >= 0 && x < 8 && y >= 0 && y < 8 {
+                                    pixels[Int(y)][Int(x)] = .red
+                                }
+                            }
+                    )
+            }
+        }
         .padding()
     }
 }
